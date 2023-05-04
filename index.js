9108f62838044bfd2ae935a16c219d87c7de5848
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-05-04 11:50:48 +0600

    Data pass

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const chef = require('./data/chef.json')
 const recipe = require('./data/recipe.json')
+
+app.use(cors())
 
 app.get('/', (req, res) =>{
     res.send('hello world bangla')
@@ -13,8 +16,20 @@ app.get('/chef', (req, res) => {
     res.send(chef)
 })
 
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chef.find(c => c._id == id);
+    res.send(selectedChef)
+})
+
 app.get('/recipe', (req, res) => {
     res.send(recipe)
+})
+
+app.get('/recipe/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedRecipe = recipe.filter(r => r._id == id);
+    res.send(selectedRecipe);
 })
 
 app.listen(port, () => {
